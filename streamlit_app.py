61a22549880c34b4bfda9e529e79397913ae7b83
Nicolas <nanaccachian@gmail.com>
2022-07-05 10:46:24 -0300

    Show the fruits selected or everything

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #Pick list
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+if (len(fruits_selected) != 0):
+    fruits_to_show = my_fruit_list.loc[fruits_selected]
+else:
+    fruits_to_show = my_fruit_list.loc[my_fruit_list]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
