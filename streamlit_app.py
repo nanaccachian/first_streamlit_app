321d32e412e4a0a3ed190e3a5c75d7c0dabfa478
Nicolas <nanaccachian@gmail.com>
2022-07-05 10:47:17 -0300

    Fix for filtering

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 if (len(fruits_selected) != 0):
     fruits_to_show = my_fruit_list.loc[fruits_selected]
 else:
-    fruits_to_show = my_fruit_list.loc[my_fruit_list]
+    fruits_to_show = my_fruit_list
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
